b6082f2aea14f91abf5fe94675bd2f2afff0ea81
Tony <ssun1@my.waketech.edu>
2026-04-10 14:15:33 -0400

    initial main

---
+++ my-skill/SKILL.md
@@ -0,0 +1,4 @@
+---
+name: my-skill
+description: A description of my skill.
+---
--- setup.sh
+++ setup.sh
@@ -4,12 +4,7 @@ set -euo pipefail
 # Change this if you want a different root folder name
 ROOT_DIR="my-skill"
 
-mkdir -p "$ROOT_DIR"/{
-  scripts,
-  references,
-  assets,
-  agents
-}
+mkdir -p "$ROOT_DIR"/{scripts,references,assets,agents}
 
 touch "$ROOT_DIR/SKILL.md"
 touch "$ROOT_DIR/agents/openai.yaml"
